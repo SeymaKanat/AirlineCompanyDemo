a8d379605e893e797456d22d87de2f6b76ce355b
Seyma <knt.seyma@gmail.com>
2023-10-17 15:13:34 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/se2/seyma/AirlineCompany/Demo/AirlineCompanyDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se2.seyma.AirlineCompany.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineCompanyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineCompanyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se2/seyma/AirlineCompany/Demo/AirlineCompanyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se2.seyma.AirlineCompany.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineCompanyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
